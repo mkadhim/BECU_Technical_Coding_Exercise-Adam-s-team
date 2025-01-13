6d604da50b2f5d3abc49fce19b8281d5436ba6bb
mohammed <mohammedbabylon84@gmail.com>
2025-01-13 15:19:17 -0500

    Add project files.

---
+++ BECU_Technical_Coding_Exercise (Adam's team)/Class.cs
@@ -0,0 +1,11 @@
+﻿namespace BECU_Technical_Coding_Exercise__Adam_s_team_
+{
+  
+        public class TaskItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public bool Completed { get; set; }
+        }
+   
+}
