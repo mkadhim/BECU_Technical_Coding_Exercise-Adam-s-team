0143c95a3122cce8b64baeaa84b4cbac39318c39
Mohammed k Kadhim <35728607+mkadhim@users.noreply.github.com>
2025-01-16 13:06:58 -0500

    Update WeatherForecast.cs

--- react api/WeatherForecast.cs
+++ react api/WeatherForecast.cs
@@ -4,6 +4,9 @@ namespace react_api
     {
         public DateTime Date { get; set; }
 
+
+        
+
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
